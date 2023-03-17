ae7d6752da25759d36de6e8a26917d2834731d0f
Deepak Pitaliya <pitaliya.d@gmail.com>
2023-03-17 18:39:37 +0530

    Stage 4 Done

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -2,9 +2,37 @@ package minesweeper
 
 import kotlin.random.Random
 
+val minesInFieldCoordinates = mutableListOf<Pair<Int, Int>>()
+val minesByPlayerCoordinates = mutableListOf<Pair<Int, Int>>()
 fun main() {
     val mineField = createMineField(9, 9)
     printMineField(mineField)
+    while (!(minesInFieldCoordinates.containsAll(minesByPlayerCoordinates) && minesByPlayerCoordinates.containsAll(
+            minesInFieldCoordinates
+        ))
+    ) {
+        println("Set/delete mines marks (x and y coordinates):")
+        val input = readln().split(" ").map { it -> it.toInt() }
+        val y = input[0] - 1
+        val x = input[1] - 1
+        setOrRemoveMark(mineField, x, y)
+    }
+    println("Congratulations! You found all the mines!")
+}
+
+fun setOrRemoveMark(mineField: Array<CharArray>, x: Int, y: Int): Array<CharArray> {
+    if (mineField[x][y] in '1'..'8') {
+        println("There is a number here!")
+    } else if (mineField[x][y] == '.') {
+        mineField[x][y] = '*'
+        minesByPlayerCoordinates.add(Pair(x, y))
+        printMineField(mineField)
+    } else {
+        mineField[x][y] = '.'
+        minesByPlayerCoordinates.remove(Pair(x, y))
+        printMineField(mineField)
+    }
+    return mineField
 }
 
 fun createMineField(rows: Int, columns: Int): Array<CharArray> {
@@ -14,6 +42,7 @@ fun createMineField(rows: Int, columns: Int): Array<CharArray> {
     val mineField = Array(rows) { CharArray(columns) { '.' } }
 
     var minesPlaced = 0
+
     while (minesPlaced < numMines) {
         val mineRow = Random.nextInt(rows)
         val mineCol = Random.nextInt(columns)
@@ -27,17 +56,26 @@ fun createMineField(rows: Int, columns: Int): Array<CharArray> {
                     }
                 }
             }
-            minesPlaced ++
+            minesPlaced++
+            minesInFieldCoordinates.add(Pair(mineRow, mineCol))
+        }
+    }
+    for (i in mineField.indices) {
+        for (j in mineField[i].indices) {
+            if (mineField[i][j] == 'X') mineField[i][j] = '.'
         }
     }
     return mineField
 }
 
 fun printMineField(mineField: Array<CharArray>) {
+    println(" │123456789│\n—│—————————│")
     for (i in mineField.indices) {
+        print("${i + 1}|")
         for (j in mineField[i].indices) {
             print(mineField[i][j])
         }
-        println()
+        println("|")
     }
+    println("—│—————————│")
 }
